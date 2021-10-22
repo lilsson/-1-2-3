d78c998b9d4ef040f407fa0e09f4d2c2eecf48fb
lilsson <pirckhelavalile@gmail.com>
2021-10-22 03:46:39 +0300

    add js

---
+++ script.js
@@ -0,0 +1,64 @@
+// task 1 
+// let  user = {
+//      firstname: "giorgi",
+//      lastname: "smith",
+//      age: 25,
+//      studentstatus: "active",
+// };
+
+// console.log (user.studentstatus);
+
+// task 2
+// 2. შექმენით მასივი, და დაბეჭდეთ სათითაოდ თითოეული ელემენტი; (for, while ციკლების საშუალებით);
+
+// let names = ["ani", "levani", "goga", "lile"];
+// for (let i = 0; i < names.length; i++) {
+//     console.log (names[i]);
+// }
+
+// let names = ["ani", "levani", "goga", "lile"];
+// let i = 0;
+// while (i < names.length){
+//     i++;
+//     console.log(names[i]);
+// }
+
+// task 3
+// 3.  შექმენით მასივი, და ამოიღეთ ყველა რიცხვი რომელიც მეტია 5ზე, for ციკლის გამოყენებით.
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for ( let i = 0; i < numbers.length; i++  ){
+//     if (numbers[i] > 5) {
+//         console.log (numbers[i]);
+//     }
+// }
+
+//  task 4 
+
+// let users =  [
+//     {username:'test1', status: false},
+//     {username:'test2', status: false},
+//     {username:'test3', status: true}
+// ];
+
+// for (let i = 0; i < users.length; i ++){
+//     if (users[i].status === true){
+//         console.log(users[i]);
+//     }
+// }
+
+// task 5
+
+// let arr = [[2, -3, 5, 11], [1, -30, -11, 100], [-1, -3, -4]];
+
+// for( let i = 0; i < arr.length; i ++){
+//     if (arr[i] > 0){
+//         console.log(arr[i]);
+//     }
+// }
+// არ მახსოვს ვერ გავაკეთე
+ 
+// task 6
+
+
